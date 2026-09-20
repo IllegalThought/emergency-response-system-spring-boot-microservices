5974fcce0b8c7217e5dbf0d09077d8c3bd5b7510
Shiv3425 <shivvashisth48@gmail.com>
2026-09-20 14:47:38 +0530

    Implement Eureka service redistry

---
+++ infrastructure/service-registry/src/test/java/com/campusnaksha/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,12 @@
+package com.campusnaksha.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
